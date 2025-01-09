1d13bc4ab0d8d28f1528d1c3d74a1ae082424dbb
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-01-09 21:21:43 +0600

    3

---
+++ 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int n;
+    cin>>n;
+    long l;
+    cin>>l;
+    char ch;
+    cin>>ch;
+    float f;
+    cin>>f;
+    double d;
+    cin>>d;
+    cout<<n<<"\n"<<l<<"\n"<<ch<<"\n"<<f<<"\n"<<d;
+    return 0;
+}
