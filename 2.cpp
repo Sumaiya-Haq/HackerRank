5160b4a81e2dbea6e9bea745ea5eb2ad80459f6c
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-01-09 20:34:01 +0600

    2

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n,a,b,c,sum;
+    cin>>a>>b>>c;
+        sum=a+b+c;
+        cout<<sum;
+    return 0;
+}
