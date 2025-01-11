9adda2be9654b8e53193ee136c75fec0e5d68786
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-01-11 22:51:20 +0600

    Basic data types in c++

--- 3.cpp
+++ 3.cpp
@@ -5,15 +5,11 @@ using namespace std;
 int main() {
     // Complete the code.
     int n;
-    cin>>n;
     long l;
-    cin>>l;
     char ch;
-    cin>>ch;
     float f;
-    cin>>f;
     double d;
-    cin>>d;
-    cout<<n<<"\n"<<l<<"\n"<<ch<<"\n"<<f<<"\n"<<d;
+   scanf("%d %ld %c %f %lf",&n,&l,&ch,&f,&d);
+   printf("%d\n %ld\n%c\n %.3f\n %.9lf",n,l,ch,f,d);
     return 0;
 }
